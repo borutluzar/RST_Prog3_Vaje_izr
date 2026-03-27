712eda885996ebdeb04e4b433ddb7b9741a22679
Borut Lužar <borut.luzar@gmail.com>
2026-03-27 17:06:16 +0100

    Add project files.

---
+++ RST_Prog3_Vaje_izr/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace RST_Prog3_Vaje_izr
+{
+    internal class Program
+    {
+        enum Tutorial
+        {
+            Tutorial_01 = 1,  // 26. 3. 2026
+            Tutorial_02 = 2,  //  8. 4. 2026
+            Tutorial_03 = 3,  // 16. 4. 2026
+            Tutorial_04 = 4,  // 22. 4. 2026
+            Tutorial_05 = 5,  // 23. 4. 2026
+            Tutorial_06 = 6,  //  5. 5. 2026
+            Tutorial_07 = 7,  // 11. 5. 2026
+        }
+
+        static void Main(string[] args)
+        {
+            switch (InterfaceFunctions.ChooseOption<Tutorial>())
+            {
+                case Tutorial.Tutorial_01:
+                    {
+                        Tutorials_01.Exercise_324();
+                    }
+                    break;
+            }
+        }
+    }
+}
---
+++ RST_Prog3_Vaje_izr/Tutorials_01.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+
+namespace RST_Prog3_Vaje_izr
+{
+    public class Tutorials_01
+    {
+        /// <summary>
+        /// Pripravite razred Index z ustreznimi lastnostmi, predvsem naj vsebuje seznam predmetov. 
+        /// Posamezen predmet naj bo instanca razreda Subject, ki ima lastnosti Name in Grade.
+        /// Lastnost Grade naj dovoli samo vrednosti med 5 in 10. 
+        /// Če poskušate nastaviti drugo vrednost, 
+        /// naj se samodejno popravi na najbližjo dovoljeno vrednost.
+        /// V razred Indeks dodajte še lastnost PovprecnaOcena kot samo za branje, 
+        /// ki izračuna povprečje ocen predmetov.
+        /// </summary>
+        public static void Exercise_324()
+        {
+            Predmet programiranje3 = new Predmet("Programiranje 3");
+            programiranje3.Ocena = 10;
+
+            Predmet modeliranje = new Predmet("3D Modeliranje");
+            modeliranje.Ocena = 10;
+
+            Index index = new Index(0);
+            index.Predmeti.Add(programiranje3);
+            index.Predmeti.Add(modeliranje);
+
+            Console.WriteLine(index);
+
+            Console.WriteLine($"Povprečje ocen študenta {index.VpisnaStevilka} je {index.PovprecnaOcena:0.00}");
+
+            Console.ReadLine();
+        }
+    }
+
+
+    /// <summary>
+    /// Razred, ki predstavlja študentski indeks.
+    /// </summary>
+    public class Index
+    {
+        public int VpisnaStevilka { get; }
+        public List<Predmet> Predmeti { get; set; } = new List<Predmet>();
+
+        public Index(int vs)
+        {
+            VpisnaStevilka = vs;
+        }
+
+        public override string ToString()
+        {
+            string izpis = $"{VpisnaStevilka} \n";
+
+            foreach (Predmet predmet in this.Predmeti)
+            {
+                izpis += $"{predmet.ToString()} \n";
+            }
+
+            return izpis;
+        }
+
+        public double PovprecnaOcena
+        {
+            get
+            {
+                return Predmeti.Average(x => x.Ocena);
+            }
+        }
+    }
+
+    public class Predmet
+    {
+        public string Ime { get; }
+
+        private int ocena;
+        public int Ocena
+        {
+            get
+            {
+                return ocena;
+            }
+            set
+            {
+                if (value < 5)
+                {
+                    ocena = 5;
+                }
+                else if (value > 10)
+                {
+                    ocena = 10;
+                }
+                else
+                {
+                    ocena = value;
+                }
+            }
+        }
+
+        public Predmet(string ime)
+        {
+            Ime = ime;
+        }
+
+        public override string ToString()
+        {
+            return $"{this.Ime} : {this.Ocena}";
+        }
+    }
+}
